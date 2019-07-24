0dae50ba43aefd38062cf546772c2a0ce31cb2b0
文科名 <i@mkev.in>
2019-07-24 15:08:38 +0800

    update

--- splice.py
+++ splice.py
@@ -2,11 +2,7 @@ from PIL import Image
 import os, sys, glob
 
 name = input('请输入图片名称：')
-size = input('请输入最终图的宽高（256x256）：')
-wh = str(size).split('x')
-width = wh[0]
-height = wh[1]
-output = Image.new('RGB', (int(width), int(height)))
+tmpImage = Image.new('RGB', (30000, 2000))
 folders = glob.glob(pathname='./images/*')
 files = glob.glob(pathname='./images/0/*.jpg')
 folderNum = len(folders)
@@ -14,25 +10,35 @@ fileNum = len(files)
 
 def main():
     widthArr = []
-    heightArr = []
     y = 0
+    width = 0
+    height = 0
     for i in range(0, folderNum):
         x = 0
+        heightArr = []
         for j in range(0, fileNum):
-            print('合并第%d行第%d张图' %(i, j))
+            print('合并第%d行，第%d张图片' %(i, j))
             read = Image.open('./images/%d/%d_%d.jpg' %(i, j, i))
-            print(read.size)
             widthArr.append(read.size[0])
             heightArr.append(read.size[1])
-            if i == 0:
-                w = 0
-                h = 0
-            else:
-                w = widthArr[i]
-                h = heightArr[j]
-            output.paste(read, (x, y))
+            w = widthArr[j]
+            h = heightArr[j]
+            tmpImage.paste(read, (x, y))
             x = x + w
         y = y + h
+        width = x
+    height = y
     print('合并结束')
-    output.save(str(name) + '.jpg')
+    print('生成临时文件')
+    tmpImage.save(str(name) + '.tmp.jpg')
+    final = Image.open(str(name) + '.tmp.jpg')
+    print('裁剪图片 '+ str(width) +'x'+ str(height))
+    finalImage = final.crop((0, 0, width, height))
+    print('保存成功')
+    finalImage.save(str(name) +'.jpg')
+    print('移除临时文件')
+    os.remove(str(name) + '.tmp.jpg')
+
 main()
+
+# 29817x1410
